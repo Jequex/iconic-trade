234db480acc7c903fa8bb36a7d47ff7104566a19
/Jequex <onyejekwe.harddisk20@gmail.com>
2023-05-12 10:52:53 +0100

    fixing endpoints

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,18 +1,9 @@
-const local = 'http://localhost:3000/api/v1';
-const dev = 'https://iconic-trades-backend.herokuapp.com/api/v1';
-const temp = '/api/v1';
-const prod = '';
-
-const current = 'local';
-
-const inUse = current === 'dev' ? dev : 'local' ? local : 'temp' ? temp : prod;
-
 export const endPoints = {
-  createAccountUrl: `${inUse}/users/user`,
-  loginUrl: `${inUse}/users/loginUser`,
-  googleLoginUrl: `${inUse}/users/googleAuth`,
-  userSubscriptionsUrl: `${inUse}/payments/userSubscriptions`,
-  verifyPaymentUrl: `${inUse}/payments/verifyPayment`,
+  createAccountUrl: `/api/v1/users/user`,
+  loginUrl: `/api/v1/users/loginUser`,
+  googleLoginUrl: `/api/v1/users/googleAuth`,
+  userSubscriptionsUrl: `/api/v1/payments/userSubscriptions`,
+  verifyPaymentUrl: `/api/v1/payments/verifyPayment`,
 };
 
 
